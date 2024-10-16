02cfd88efed27b8138509b4e1304132b78a7f895
Liban Yonis <assadyonis@gmail.com>
2024-10-16 18:59:58 +0300

    first commit of uber clone (DjibRyde) app

---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,12 @@
+import { Text, View, SafeAreaView } from "react-native";
+// import { SafeAreaView } from "react-native-safe-area-context";
+
+const Rides = () => {
+  return (
+    <View>
+      <Text>Rides</Text>
+    </View>
+  );
+};
+
+export default Rides;
